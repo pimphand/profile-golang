a2841dcd16e3c89e6b7bec91e54941df4a5f0b07
pimphand <faisaldwiki69@gmail.com>
2024-09-30 17:50:36 +0700

    add domain

--- routes/web.go
+++ routes/web.go
@@ -9,7 +9,7 @@ import (
 func InitRoute(app *gin.Engine) {
 	route := app
 	route.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, // Update this to the origin of your Vue.js app
+		AllowOrigins:     []string{"http://localhost:5173", "dashboard.naisha.id"}, // Update this to the origin of your Vue.js app
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "authorization", "Access-Control-Allow-Origin"},
 		ExposeHeaders:    []string{"Content-Length"},
