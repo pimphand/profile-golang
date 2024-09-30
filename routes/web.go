f7d580f45f220327250c7df98fe316719e59e7cb
pimphand <faisaldwiki69@gmail.com>
2024-09-30 21:03:47 +0700

    f

--- routes/web.go
+++ routes/web.go
@@ -2,20 +2,13 @@ package routes
 
 import (
 	"gin-gonic-gorm/controllers/product_controller"
-	"github.com/gin-contrib/cors"
+
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoute(app *gin.Engine) {
 	route := app
-	route.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "dashboard.naisha.id"}, // Update this to the origin of your Vue.js app
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "authorization", "Access-Control-Allow-Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * 3600, // Optional: cache preflight response for 12 hours
-	}))
+
 	//login
 	route.GET("/login", func(c *gin.Context) {
 		c.JSON(200, gin.H{
